5e79f9c6b2a23f7bb29c9f165ab9ec9e5448d22b
Munawar Ali <munawar.ali@careem.com>
2021-04-28 03:49:15 +0500

    First commit. Adding project created through spring.initilizer.io

---
+++ src/test/java/com/labfarward/programmingtest/ProgrammingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labfarward.programmingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
