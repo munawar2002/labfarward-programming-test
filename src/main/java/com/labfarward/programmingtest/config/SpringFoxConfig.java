e2060e7be4252208f60f86bd695e6eef0a6ce36b
Munawar Ali <munawar.ali@careem.com>
2021-05-01 13:34:58 +0500

    Updating the API documentation

--- src/main/java/com/labfarward/programmingtest/config/SpringFoxConfig.java
+++ src/main/java/com/labfarward/programmingtest/config/SpringFoxConfig.java
@@ -8,9 +8,15 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,7 +27,18 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.springframework.boot").negate())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        final Contact owner = new Contact("LabForward", "https://www.labforward.com/", "info@labforward.com");
+
+        return new ApiInfo("LabForward Item Category Service",
+                "This API is for Scientists that need a digital solution to store and identify various items " +
+                        "used in their experiments. These items come in different forms and might include samples," +
+                        " chemicals, devices, etc...", "1.0",
+                "termsOfServiceUrl", owner, "license", "licenseUrl", new ArrayList<>());
     }
 
     @Override
